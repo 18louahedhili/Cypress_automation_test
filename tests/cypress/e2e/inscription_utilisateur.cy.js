57c3d9e25199d54c07c9019c443069a2615fdd31
Loua <128370594+18louahedhili@users.noreply.github.com>
2023-05-17 12:12:50 +0200

    Add new test case:Failed Registration

--- tests/cypress/e2e/inscription_utilisateur.cy.js
+++ tests/cypress/e2e/inscription_utilisateur.cy.js
@@ -60,4 +60,62 @@ describe('User Registration - Individual', () => {
     cy.get('button.btn.btn-primary').contains('Créer votre compte').click()
 
     })
+
+
+
+    it('prevents users from registering without filling in all required fields', () => {
+      cy.visit('https://opencruise-ok.sogeti-center.cloud/register')
+
+      // Step 1: Click on the "Particulier" button
+    cy.get('input[name="type"][value="1"]').click()
+
+    
+    // Step 2: Fill in the registration form with valid user details
+
+    // Fill in the first name
+    cy.get('input[formcontrolname="prenom"]:eq(0)').type('Smith')
+
+    // Fill in the last name
+    cy.get('input[formcontrolname="nom"]:eq(0)').type('Olivier')
+
+    // Fill in the date of birth
+    cy.get('input[formcontrolname="dateNaissance"]:eq(0)').type('1990-05-22')
+
+    // Fill in the address
+    cy.get('input[formcontrolname="adresse"]:eq(0)').type('France')
+
+    // Select the country
+    cy.get('select[formcontrolname="currentPays"]:eq(0)').select('Canada')
+
+    // Select the city
+    cy.get('select[formcontrolname="ville"]:eq(0)').select('Vancouver')
+
+    // Fill in the postal code
+    cy.get('input[formcontrolname="codePostal"]:eq(0)').type('75014')
+
+    // Fill in the passport number
+    cy.get('input[formcontrolname="passport"]:eq(0)').type('0123456789')
+
+    // Fill in the CIN number
+    cy.get('input[formcontrolname="cin"]:eq(0)').type('0202020202')
+
+    // Fill in the username (email)
+    cy.get('input[formcontrolname="username"]:eq(0)').type('olivier@test.com')
+
+    // Do not fill in the phone number field
+    //--------------------------------------
+
+    // Fill in the password
+    cy.get('input[formcontrolname="password"]:eq(0)').type('Olivier12345')
+
+    // Confirm the password
+    cy.get('input[formcontrolname="confirmPassword"]:eq(0)').type('Olivier12345')
+
+    // Click on the "Créer votre compte" button
+    cy.get('button.btn.btn-primary').contains('Créer votre compte').click()
+
+    // Assert that an error message is displayed indicating the missing field
+    cy.contains('merci de saisir le téléphone').should('be.visible')
+  
   })
+})
