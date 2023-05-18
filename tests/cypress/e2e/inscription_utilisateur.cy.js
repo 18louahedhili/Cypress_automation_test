35679bdeccd82b050f38720748809e8828018ae3
Loua <128370594+18louahedhili@users.noreply.github.com>
2023-05-18 21:27:21 +0200

    Refactoring : Update inscription test case

--- tests/cypress/e2e/inscription_utilisateur.cy.js
+++ tests/cypress/e2e/inscription_utilisateur.cy.js
@@ -1,133 +1,123 @@
+import InscriptionUtilisateurSteps from './steps/Inscription_utilisateur_steps.js';
+const inscriptionUtilisateurSteps = new InscriptionUtilisateurSteps();
+
+
 describe('User Registration - Individual', () => {
-  it('allows users to register successfully on the site', () => {
+
+  beforeEach(() => {
     cy.visit('https://opencruise-ok.sogeti-center.cloud')
 
-    // Step 1: Click on the "Vous n'avez pas de compte ? Créez-en un ici" link
-    cy.get('a[routerlink="/register"]').contains('Vous n\'avez pas de compte ? Créez-en un ici').click()
+    inscriptionUtilisateurSteps.createAccount()
 
-    // Step 2: Verify that the user is redirected to the registration page
-    cy.url().should('eq', 'https://opencruise-ok.sogeti-center.cloud/register')
+    inscriptionUtilisateurSteps.checkRegisterPage()
 
-    // Step 3: Click on the "Particulier" button
-    cy.get('input[name="type"][value="1"]').click()
+    inscriptionUtilisateurSteps.validateParticularAccount()
 
-    
-    // Step 4: Fill in the registration form with valid user details
-
-    // Fill in the first name
-    cy.get('input[formcontrolname="prenom"]:eq(0)').type('Doe')
-
-    // Fill in the last name
-    cy.get('input[formcontrolname="nom"]:eq(0)').type('Sarra')
-
-    // Fill in the date of birth
-    cy.get('input[formcontrolname="dateNaissance"]:eq(0)').type('1990-05-22')
-
-    // Fill in the address
-    cy.get('input[formcontrolname="adresse"]:eq(0)').type('France')
-
-    // Select the country
-    cy.get('select[formcontrolname="currentPays"]:eq(0)').select('Canada')
-
-    // Select the city
-    cy.get('select[formcontrolname="ville"]:eq(0)').select('Vancouver')
-
-    // Fill in the postal code
-    cy.get('input[formcontrolname="codePostal"]:eq(0)').type('75002')
-
-    // Fill in the passport number
-    cy.get('input[formcontrolname="passport"]:eq(0)').type('012345678')
-
-    // Fill in the CIN number
-    cy.get('input[formcontrolname="cin"]:eq(0)').type('0101010101')
-
-    // Fill in the username (email)
-    cy.get('input[formcontrolname="username"]:eq(0)').type('doesarra@test.com')
-
-    // Fill in the phone number
-    cy.get('input[formcontrolname="tel"]:eq(0)').type('012345678')
-
-    // Fill in the password
-    cy.get('input[formcontrolname="password"]:eq(0)').type('Sarra12345')
-
-    // Confirm the password
-    cy.get('input[formcontrolname="confirmPassword"]:eq(0)').type('Sarra12345')
-
-    // Fill in the spouse's last name
-    cy.get('input[formcontrolname="nomConjoint"]:eq(0)').type('Langard')
-
-    // Fill in the spouse's first name
-    cy.get('input[formcontrolname="prenomConjoint"]:eq(0)').type('Pierre')
-
-    // Fill in the spouse's date of birth
-    cy.get('input[formcontrolname="dateNaissanceConjoint"]:eq(0)').type('1984-08-15')
-
-    // Step 5:Click on the "Créer votre compte" button
-    cy.get('button.btn.btn-primary').contains('Créer votre compte').click()
-
-    })
-
-
-
-    it('prevents users from registering without filling in all required fields', () => {
-      cy.visit('https://opencruise-ok.sogeti-center.cloud')
-
-    // Step 1: Click on the "Vous n'avez pas de compte ? Créez-en un ici" link
-    cy.get('a[routerlink="/register"]').contains('Vous n\'avez pas de compte ? Créez-en un ici').click()
-
-    // Step 2: Verify that the user is redirected to the registration page
-    cy.url().should('eq', 'https://opencruise-ok.sogeti-center.cloud/register')
-
-    // Step 3: Click on the "Particulier" button
-    cy.get('input[name="type"][value="1"]').click()
-
-    
-    // Step 4: Fill in the registration form with valid user details
-
-    // Fill in the first name
-    cy.get('input[formcontrolname="prenom"]:eq(0)').type('Smith')
-
-    // Fill in the last name
-    cy.get('input[formcontrolname="nom"]:eq(0)').type('Olivier')
-
-    // Fill in the date of birth
-    cy.get('input[formcontrolname="dateNaissance"]:eq(0)').type('1990-05-22')
-
-    // Fill in the address
-    cy.get('input[formcontrolname="adresse"]:eq(0)').type('France')
-
-    // Select the country
-    cy.get('select[formcontrolname="currentPays"]:eq(0)').select('Canada')
-
-    // Select the city
-    cy.get('select[formcontrolname="ville"]:eq(0)').select('Vancouver')
-
-    // Fill in the postal code
-    cy.get('input[formcontrolname="codePostal"]:eq(0)').type('75014')
-
-    // Fill in the passport number
-    cy.get('input[formcontrolname="passport"]:eq(0)').type('0123456789')
-
-    // Fill in the CIN number
-    cy.get('input[formcontrolname="cin"]:eq(0)').type('0202020202')
-
-    // Fill in the username (email)
-    cy.get('input[formcontrolname="username"]:eq(0)').type('olivier@test.com')
-
-    // Do not fill in the phone number field
-    //--------------------------------------
-
-    // Fill in the password
-    cy.get('input[formcontrolname="password"]:eq(0)').type('Olivier12345')
-
-    // Confirm the password
-    cy.get('input[formcontrolname="confirmPassword"]:eq(0)').type('Olivier12345')
-
-    // Click on the "Créer votre compte" button
-    cy.get('button.btn.btn-primary').contains('Créer votre compte').click()
-
-    // Step 5:Assert that an error message is displayed indicating the missing field
-    cy.contains('merci de saisir le téléphone').should('be.visible')
-  
   })
+
+  it('allows users to register successfully on the site', () => {
+
+    // Step 4: Fill in the registration form with valid user details
+    // Fill in the first name
+    inscriptionUtilisateurSteps.setFormulaire('Doe',
+      'Sarra',
+      '1990-05-22',
+      'France',
+      'Canada',
+      'Vancouver',
+      '75002',
+      '012345678',
+      '0101010101',
+      'doesarra@test.com',
+      '012345678',
+      'Sarra12345',
+      'Sarra12345',
+      'Langard',
+      'Pierre',
+      '1984-08-15'
+    )
+    inscriptionUtilisateurSteps.valider()
+
+  })
+
+  it('prevents users from registering without filling in all required fields', () => {
+
+    inscriptionUtilisateurSteps.setFormulaire('Smith',
+      'Olivier',
+      '1990-05-22',
+      'France',
+      'Canada',
+      'Vancouver',
+      '75014',
+      '0123456789',
+      '0202020202',
+      'olivier@test.com',
+      '',
+      'Olivier12345',
+      'Olivier12345',
+      '',
+      '',
+      ''
+    )
+    inscriptionUtilisateurSteps.valider()
+    // Step 5:Assert that an error message is displayed indicating the missing field
+    inscriptionUtilisateurSteps.checkError('merci de saisir le téléphone')
+  })
+
+  it('prevents users from registering without confirmation his password', () => {
+
+    inscriptionUtilisateurSteps.setFormulaire('Smith',
+      'Olivier',
+      '1990-05-22',
+      'France',
+      'Canada',
+      'Vancouver',
+      '75014',
+      '0123456789',
+      '0202020202',
+      'olivier@test.com',
+      '0606060606',
+      'Olivier12345',
+      'Olivier1234',
+      '',
+      '',
+      ''
+    )
+    inscriptionUtilisateurSteps.valider()
+    // Step 5:Assert that an error message is displayed indicating the missing field
+    inscriptionUtilisateurSteps.checkError('les mots de passe doivent être identiques')
+  })
+
+  it('prevents users from registering without typing informations', () => {
+
+    inscriptionUtilisateurSteps.setFormulaire('',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      ''
+    )
+    inscriptionUtilisateurSteps.valider()
+    // Step 5:Assert that an error message is displayed indicating the missing field
+    inscriptionUtilisateurSteps.checkError('merci de saisir votre prénom')
+    inscriptionUtilisateurSteps.checkError('merci de saisir votre nom')
+    inscriptionUtilisateurSteps.checkError('merci de saisir la date de naissance')
+    inscriptionUtilisateurSteps.checkError('merci de saisir votre adresse')
+    inscriptionUtilisateurSteps.checkError('merci de saisir le Pays')
+    inscriptionUtilisateurSteps.checkError('merci de saisir la ville')
+    inscriptionUtilisateurSteps.checkError('merci de saisir le code postal')
+    inscriptionUtilisateurSteps.checkError('merci de saisir le numéro de passeport')
+    inscriptionUtilisateurSteps.checkError('merci de saisir le Numéro de carte d’identité')
+  })
+
 })
