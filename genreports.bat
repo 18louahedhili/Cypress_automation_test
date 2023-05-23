faefa7074e01cd083da89452490a81c71179e67b
Loua <128370594+18louahedhili@users.noreply.github.com>
2023-05-23 12:24:44 +0200

    reports

--- genreports.bat
+++ genreports.bat
@@ -1,2 +1,7 @@
 cd tests
 node_modules\.bin\cypress run combine-reports > ../doc/execution.md
+echo (![execution_connexion](../tests/cypress/videos/connexion_utilisateur.cy.js.mp4) ) > ../doc/execution.md
+echo (![execution_inscription_ko](../tests/cypress/videos/inscription_utilisateur_ko.cy.js.mp4) )> ../doc/execution.md
+echo (![execution_inscription](../tests/cypress/videos/inscription_utilisateur.cy.js.mp4)) > ../doc/execution.md
+echo (![execution_recherche](../tests/cypress/videos/recherche_croisiere.cy.js.mp4)) > ../doc/execution.md
+echo ("END")
